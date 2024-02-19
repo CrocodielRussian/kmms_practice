5643bdf441327b0feb5fe707a850e69903891b60
CrocodielRussian <mk-spbkiselev@yandex.ru>
2024-02-19 13:49:34 +0300

    Add refactoting project.

--- build.bat
+++ build.bat
@@ -21,10 +21,12 @@ cmake --build
 
 set arr[0].file=run_bubble_sort.bat
 set arr[1].file=run_bubble_sort_split.bat
+set arr[2].file=run_refactoring_proj.bat
 
 set arr[0].folder=bubble_sort
 set arr[1].folder=bubble_sort_split
+set arr[2].folder=refactoring_proj
 
-for /L %%i in (0,1,1) do ( 
+for /L %%i in (0,1,2) do ( 
 	copy ..\%SOURCE_FOLDER%\!arr[%%i].folder!\!arr[%%i].file! .\!arr[%%i].folder!
 )
