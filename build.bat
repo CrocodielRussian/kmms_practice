8d14df74fa1f1d4070d3770fba98c69f7c6b6400
CrocodielRussian <mk-spbkiselev@yandex.ru>
2024-02-20 19:41:46 +0300

    Add my_project

--- build.bat
+++ build.bat
@@ -23,13 +23,15 @@ set arr[0].file=run_bubble_sort.bat
 set arr[1].file=run_bubble_sort_split.bat
 set arr[2].file=run_refactoring_proj.bat
 set arr[3].folder=hello.bat
+set arr[4].folder=my_project.bat
 
 set arr[0].folder=bubble_sort
 set arr[1].folder=bubble_sort_split
 set arr[2].folder=refactoring_proj
 set arr[3].folder=hello
+set arr[4].folder=my_project
 
 
-for /L %%i in (0,1,3) do ( 
+for /L %%i in (0,1,4) do ( 
 	copy ..\%SOURCE_FOLDER%\!arr[%%i].folder!\!arr[%%i].file! .\!arr[%%i].folder!
 )
---
+++ projects/my_project/my_project.bat
@@ -0,0 +1,6 @@
+@echo off
+
+chcp 65001 > log
+del log
+
+my_project.exe
